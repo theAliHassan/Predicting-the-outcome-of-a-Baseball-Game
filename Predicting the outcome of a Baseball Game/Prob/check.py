d933228246674785868c35f835fa26541f0a6899
theAliHassan <76556926+theAliHassan@users.noreply.github.com>
2022-03-21 18:54:07 +0500

    Add files via upload

---
+++ Predicting the outcome of a Baseball Game/Prob/check.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from tkinter import font
+import main.py
+
+
+root=Tk()
+
+#gui logic
+
+
+root.geometry('400x250')
+root.title('Learning')
+
+
+
+#label options 
+# text
+# bd
+# fg
+# font
+# padx
+# pady
+# relief - border styling sunken , raised , groove , ridge 
+#  
+
+#pack options
+# anchor = nw
+# side 
+# fill
+# padx
+# pady
+
+
+
+root.mainloop()
+
+
